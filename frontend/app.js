3f8680ba1b524c38e34f60632119eb6ef399ecf5
Khushi6378 <khushisharma80294@gmail.com>
2025-03-21 15:13:13 +0530

    Add files via upload

---
+++ frontend/app.js
@@ -0,0 +1,188 @@
+const contractAddress = "0x32CFaFc4bA244E3624d5dB129F83838c7299aCfe";
+const abi = [[
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "NFTListed",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "creator",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"internalType": "string",
+				"name": "tokenURI",
+				"type": "string"
+			}
+		],
+		"name": "NFTMinted",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"internalType": "address",
+				"name": "buyer",
+				"type": "address"
+			}
+		],
+		"name": "NFTSold",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			}
+		],
+		"name": "buyNFT",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "listNFT",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "tokenURI",
+				"type": "string"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			}
+		],
+		"name": "mintNFT",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "nftCollection",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "tokenId",
+				"type": "uint256"
+			},
+			{
+				"internalType": "address payable",
+				"name": "creator",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "price",
+				"type": "uint256"
+			},
+			{
+				"internalType": "bool",
+				"name": "forSale",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]];
+
+let web3;
+let contract;
+
+async function connectBlockchain() {
+    if (window.ethereum) {
+        web3 = new Web3(window.ethereum);
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        contract = new web3.eth.Contract(abi, contractAddress);
+        loadNFTs();
+    } else {
+        alert("Please install MetaMask!");
+    }
+}
+
+async function mintNFT() {
+    const tokenURI = document.getElementById("tokenURI").value;
+    const price = document.getElementById("nftPrice").value;
+    const accounts = await web3.eth.getAccounts();
+    
+    await contract.methods.mintNFT(tokenURI, web3.utils.toWei(price, "ether"))
+        .send({ from: accounts[0] });
+
+    alert("NFT Minted!");
+    loadNFTs();
+}
+
+async function loadNFTs() {
+    const nftList = document.getElementById("nft-list");
+    nftList.innerHTML = "";
+
+    for (let i = 1; i <= 10; i++) { // Mock loading NFTs
+        let nftDiv = document.createElement("div");
+        nftDiv.innerHTML = `<p>NFT #${i}</p><button>Buy</button>`;
+        nftList.appendChild(nftDiv);
+    }
+}
+
+window.onload = connectBlockchain;
